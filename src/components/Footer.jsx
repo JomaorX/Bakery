836465b98acee903e1c0b70b7192522cd3a975e1
JomaorX <martinez97.pro@gmail.com>
2025-02-11 13:35:06 +0100

    adding styles 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,11 @@ function Footer() {
     return (
         <footer>
             <p>© 2024 Pastelería Delicias. Todos los derechos reservados.</p>
+            <div class="social-media">
+        <a href="https://www.facebook.com/" target="_blank"><img src="images/f.png" alt="Facebook"/></a>
+        <a href="https://x.com/?lang=es" target="_blank"><img src="images/x.png" alt="Twitter"/></a>
+        <a href="https://www.instagram.com/?hl=es" target="_blank"><img src="images/i.png" alt="Instagram"/></a>
+    </div>
         </footer>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <img src="/images/logo.png" alt="logo Dulce Delicias" />
-            <ul>
-                <li><Link to="/">Inicio</Link></li>
+        <nav class="navbar">
+        <div class="logo">
+            <img src="images/logo.png" alt="logotipo"/>
+        </div>
+        <ul>
+        <li><Link to="/">Inicio</Link></li>
                 <li><Link to="/productos">Productos</Link></li>
                 <li><Link to="/contacto">Contacto</Link></li>
-            </ul>
-        </nav>
+        </ul>
+    </nav>
     );
 }
 
