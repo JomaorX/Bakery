f592cb3b1832ecab5055b11539859b16f0677458
JomaorX <martinez97.pro@gmail.com>
2025-02-11 12:32:17 +0100

    Comienzo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Partelería Delicas</h1>
+            <ul>
+                <li><Link to="/"></Link>Inicio</li>
+                <li><Link to="/productos">Productos</Link></li>
+                <li><Link to="/contacto"></Link>Contacto</li>
+            </ul>
+        </nav> >
+)
+}
+
+export default Navbar
