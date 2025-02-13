cb9c26eb28bef1142e941c06f4c15445be786507
JomaorX <martinez97.pro@gmail.com>
2025-02-13 20:00:59 +0100

    Permitir dominio en Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173,
-    host: true
-  },
+    port: process.env.PORT || 3000, // Usa el puerto asignado por Render o 3000 por defecto
+    host: true, // Permite acceso externo
+    allowedHosts: ["bakery-m4by.onrender.com"] // Agrega el dominio de Render
+  }
 })
